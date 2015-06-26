2f3d4d4e926f79f6ed935bbb2130ab9b7b960fe6
Raffaello <raffaello.bonghi@minisumo.net>
2015-06-26 14:13:22 +0200

    Added EEPROM and PCF8574 in according with new reference https://github.com/officinerobotiche/uNAV.X/pull/48

---
+++ includes/peripherals/i2c/i2c.h
@@ -0,0 +1,24 @@
+/* 
+ * File:   allperipherals.h
+ * Author: Raffaello
+ *
+ * Created on 26 giugno 2015, 14.03
+ */
+
+#ifndef ALLPERIPHERALS_H
+#define	ALLPERIPHERALS_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+#include "eeprom.h"
+#include "led_pcf8574.h"
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* ALLPERIPHERALS_H */
+
